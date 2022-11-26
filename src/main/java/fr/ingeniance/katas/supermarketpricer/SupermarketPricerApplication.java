5ddf03e6c6c50945c6b349ee877ad7a4ea7e90f8
Achraf RHARISS <rhariss.achraf@gmail.com>
2022-11-26 16:01:46 +0100

    init: initialization of the project

---
+++ src/main/java/fr/ingeniance/katas/supermarketpricer/SupermarketPricerApplication.java
@@ -0,0 +1,13 @@
+package fr.ingeniance.katas.supermarketpricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketPricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketPricerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ingeniance/katas/supermarketpricer/SupermarketPricerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ingeniance.katas.supermarketpricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketPricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
